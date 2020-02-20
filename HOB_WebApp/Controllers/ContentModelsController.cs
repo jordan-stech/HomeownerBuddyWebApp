96a246dd23c66caa5932d9bdd4ab09603db2f1b1
staciecarroll <carroll.663@osu.edu>
2020-02-20 14:41:29 -0500

    create issue page done

--- HOB_WebApp/Controllers/ContentModelsController.cs
+++ HOB_WebApp/Controllers/ContentModelsController.cs
@@ -25,6 +25,12 @@ namespace HOB_WebApp.Controllers
             return View(await _context.ContentModel.ToListAsync());
         }
 
+        // GET: ContentModels/CreateIssue
+        public IActionResult CreateIssue()
+        {
+            return View();
+        }
+
         // GET: ContentModels/Details/5
         public async Task<IActionResult> Details(int? id)
         {
