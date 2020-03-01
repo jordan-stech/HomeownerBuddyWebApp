98a64204ef828691d437356a8b9b3a8b56bb0fcd
staciecarroll <carroll.663@osu.edu>
2020-03-01 17:45:00 -0500

    mobile users added to website

---
+++ HOB_WebApp/Models/MobileUsers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HOB_WebApp.Models
+{
+    public class MobileUsers
+    {
+        public int Id { get; set; }
+        public string FName { get; set; }
+        public string Lname { get; set; }
+        public string HomeCode { get; set; }
+    }
+}
