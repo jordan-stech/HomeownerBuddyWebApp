4c2071c5280751fa81e5b3de17e40f47b17d642c
staciecarroll <carroll.663@osu.edu>
2020-04-12 23:56:45 -0400

    home code table created, mobile users uses the table in api

---
+++ HOB_WebApp/Models/HomeCodes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HOB_WebApp.Models
+{
+    public class HomeCodes
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Address { get; set; }
+    }
+}
