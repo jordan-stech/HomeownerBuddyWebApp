badff4af62c6f239e25cf038cfc02f21433585ba
jordan-stech <jtstech93@gmail.com>
2020-06-16 22:14:53 -0400

    The UI for the Maintenace Reminder tab/pages are finished (for now). Basic functionality is there as well for editing, viewing and deleting entries in the DB. There is also an API for the Maintenance Reminder page now.
    Creating doesn't work correctly yet as well as the search function but the views are there.

---
+++ HOB_WebApp/Controllers/MaintenanceReminderAPIController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HOB_WebApp.Data;
+using Microsoft.AspNetCore.Authorization;
+
+namespace HOB_WebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [AllowAnonymous]
+    public class MaintenanceReminderAPIController : Controller
+    {
+        private readonly HOB_WebAppContext _context;
+
+        //Get the DB context
+        public MaintenanceReminderAPIController(HOB_WebAppContext context)
+        {
+            _context = context;
+        }
+
+
+        /**
+         * This is what we call to return a JSON of every Maintenance Reminder in the DB
+         **/
+        //When a "GET" request received, return all Maintenance Reminders
+        [HttpGet]
+        // GET: Issue
+        public IActionResult Index()
+        {
+            return Json(_context.MaintenanceReminders);
+        }
+    }
+}
--- HOB_WebApp/Models/MaintenanceReminders.cs
+++ HOB_WebApp/Models/MaintenanceReminders.cs
@@ -10,12 +10,9 @@ namespace HOB_WebApp.Models
     {
         //Auto generated ID for the plan
         public int Id { get; set; }
-
         //The title of the maintenance reminder task
-        //[Required]
         public string Reminder { get; set; }
         //The category which the maintenance task belongs to
-        //[Required]
         public string Sent { get; set; }
         //Check to see if a homeowner completed a maintenance task
         public string Completed { get; set; }
