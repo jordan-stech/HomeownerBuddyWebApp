f6a2edc22cc6160e97d065febd1c937e0248395c
jordan-stech <jtstech93@gmail.com>
2020-07-15 16:21:50 -0400

    A separate thread and timer run in the background calling the BackgroundAPIController to update due dates for each user now.

--- HOB_WebApp/Program.cs
+++ HOB_WebApp/Program.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace HOB_WebApp
 {
@@ -16,8 +18,11 @@ namespace HOB_WebApp
         public static void Main(string[] args)
         {
             // Thread Created
-            Thread t = new Thread(new ThreadStart(TimerThreadProc));
+            Thread t = new Thread(new ThreadStart(FirebaseTimerThreadProc));
             t.Start();
+
+            Thread t2 = new Thread(new ThreadStart(DueDateTimerThreadProc));
+            t2.Start();
 
             CreateHostBuilder(args).Build().Run();
         }
@@ -29,25 +34,43 @@ namespace HOB_WebApp
                     webBuilder.UseStartup<Startup>();
                 });
 
-        public static void TimerThreadProc()
+        public static void FirebaseTimerThreadProc()
         {
             int timeval = 1800 * 1000;
             System.Timers.Timer timer1 = new System.Timers.Timer
             {
                 Interval = timeval
             };
-            timer1.Elapsed += OnTimedEvent;
+            timer1.Elapsed += OnTimedEvent1;
             timer1.AutoReset = true;
             timer1.Enabled = true;
         }
 
-        private static async void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        public static void DueDateTimerThreadProc()
         {
-            await ApiCall();
+            int timeval = 3600000;
+            System.Timers.Timer timer1 = new System.Timers.Timer
+            {
+                Interval = timeval
+            };
+            timer1.Elapsed += OnTimedEvent2;
+            timer1.AutoReset = true;
+            timer1.Enabled = true;
+        }
+
+        private static async void OnTimedEvent1(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            await FirebaseApiCall();
 
         }
 
-        public static async Task ApiCall()
+        private static async void OnTimedEvent2(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            await DueDateApiCall();
+
+        }
+
+        public static async Task FirebaseApiCall()
         {
             // Set up new HttpClientHandler and its credentials so we can perform the web request
             HttpClientHandler clientHandler = new HttpClientHandler();
@@ -65,6 +88,35 @@ namespace HOB_WebApp
             var getResponse = await httpClient.GetAsync(uri);
 
             if(getResponse.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Success");
+            }
+        }
+
+        public static async Task DueDateApiCall()
+        {
+            string userId = "";
+
+            // Set up new HttpClientHandler and its credentials so we can perform the web request
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            // Create new httpClient using our client handler created above
+            HttpClient httpClient = new HttpClient(clientHandler);
+
+            String apiUrl = "https://habitathomeownerbuddy.azurewebsites.net/api/BackgroundAPI/" + userId;
+
+            // Create new URI with the API url so we can perform the web request
+            var uri = new Uri(string.Format(apiUrl, string.Empty));
+
+            string JSONresult = JsonConvert.SerializeObject(userId);
+            Console.WriteLine(JSONresult);
+
+            var content = new StringContent(JSONresult, Encoding.UTF8, "application/json");
+
+            var putResponse = await httpClient.PutAsync(uri, content);
+
+            if (putResponse.IsSuccessStatusCode)
             {
                 Console.WriteLine("Success");
             }
